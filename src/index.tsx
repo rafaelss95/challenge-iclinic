9a376a5d16bf38c2b5da838b047c4c57e6ad245a
Rafael Santana <rafaelsantana3095@gmail.com>
2021-08-01 11:56:59 -0300

    build: msw configuration

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+async function bootstrap() {
+  if (process.env.NODE_ENV === 'development') {
+    const { worker } = require('./servers/dev-server');
+    await worker.start({
+      onUnhandledRequest: 'bypass',
+    });
+  }
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
+  );
+}
+
+bootstrap();

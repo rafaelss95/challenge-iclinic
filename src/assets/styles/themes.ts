5edf8b6238fd0c7de21004eb421230890e9abb05
Rafael Santana <rafaelsantana3095@gmail.com>
2021-08-01 12:13:21 -0300

    build: styled-components configuration

---
+++ src/assets/styles/themes.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+export const darkTheme: DefaultTheme = {
+  title: 'dark',
+  backgroundColor: '#2A2A2A',
+  textColor: '#FFFFFF',
+};
+
+export const lightTheme: DefaultTheme = {
+  title: 'light',
+  backgroundColor: '#FBFE63',
+  textColor: '#2A2A2A',
+};
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,5 @@
 import '@testing-library/jest-dom';
+import 'jest-styled-components';
 import { server } from './servers/test-server';
 
 beforeAll(() => server.listen());
